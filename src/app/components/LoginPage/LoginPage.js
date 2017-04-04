d1751bde6e30ab1dbb5c024868fa20e29abb7ef2
LeonYuAng3NT <leonzhang1996@hotmail.com>
2017-04-04 06:28:34 -0400

    change database and facebook-login

--- src/app/components/LoginPage/LoginPage.js
+++ src/app/components/LoginPage/LoginPage.js
@@ -110,7 +110,7 @@ export default class LoginPage extends React.Component {
                 <FormGroup>
                     <Col smOffset={2} sm={10}>
                         <FacebookLogin
-                            appId="1734208480228199"
+                            appId="412857565736679"
                             size="medium"
                             autoLoad={false}
                             fields="name,email,picture"
